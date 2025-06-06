425fabcee33ea3825928e81e81314f7c6b784c54
palakshah09 <143188652+palakshah09@users.noreply.github.com>
2025-06-06 17:00:50 +0530

    Update ProductDisplay.jsx

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
-3
+
 const ProductDisplay = (props) => {
     const {product} = props;
   return (
